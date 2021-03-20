ac3585b72585439268c0a9868150938922a68647
GCAdigwe <gcadigwe@gmail.com>
2021-03-20 17:05:44 +0000

    started working on header

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfqsRag5NFnmEzV4nY-SFAabwQLDe-DL8",
+  authDomain: "instagram-clone-33f2c.firebaseapp.com",
+  projectId: "instagram-clone-33f2c",
+  storageBucket: "instagram-clone-33f2c.appspot.com",
+  messagingSenderId: "872575980011",
+  appId: "1:872575980011:web:dae2adec967c62f139c9ae",
+  measurementId: "G-P5XM4LTK7Q",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
