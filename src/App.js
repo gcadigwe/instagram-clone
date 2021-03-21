8563c9979fd28946d718f1eaa317e09e27bb751f
GCAdigwe <gcadigwe@gmail.com>
2021-03-21 19:43:59 +0000

    post component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Post from "./components/Post/Post";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Post />
     </div>
   );
 }
